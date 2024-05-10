ac7d05fe75681c586485896c94cfb60c149bf2f5
taharidvanozturk <taharidvanozturk0@gmail.com>
2024-05-11 01:14:53 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Grup 4 Yazlab 2"
+include(":app")
+ 
